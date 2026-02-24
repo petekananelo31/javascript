0ea53451489bb791f3e57889a747d7a9630395a5
Kananelo <144343631+petekananelo31@users.noreply.github.com>
2026-02-24 10:20:54 +0200

    folder for git

---
+++ traning.js
@@ -0,0 +1,14 @@
+let myVariable = 4;
+console.log(myVariable);
+
+/*
+let firstName = "John";
+let lastName = "Doe";
+let fullName = firstName + " " + lastName;
+console.log(fullName);
+*/
+
+let greeting = "Hello, ";
+let name = "Alice";
+greeting += name; // This is the same as greeting = greeting + name;
+console.log(greeting);
