de11af70be0cdb6d250864107bbcd5105e3ffe31
Kananelo <144343631+petekananelo31@users.noreply.github.com>
2026-02-24 10:49:20 +0200

    traning

--- traning.js
+++ traning.js
@@ -1,5 +1,5 @@
-let myVariable = 4;
-console.log(myVariable);
+//let myVariable = 4;
+//console.log(myVariable);
 
 /*
 let firstName = "John";
@@ -8,7 +8,15 @@ let fullName = firstName + " " + lastName;
 console.log(fullName);
 */
 
+/*
 let greeting = "Hello, ";
 let name = "Alice";
 greeting += name; // This is the same as greeting = greeting + name;
 console.log(greeting);
+*/
+
+let str1 = 'Hello';
+let str2 = 'World';
+
+let result = str1.concat(' ', str2); 
+console.log(result); // Hello World
