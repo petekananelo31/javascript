a3df1e585a85fc23c84e0f64e88abfc2c4075bf6
Kananelo <petekananelo31@gmail.com>
2026-07-08 11:42:53 +0200

    backend developer

--- traning.js
+++ traning.js
@@ -14,9 +14,31 @@ let name = "Alice";
 greeting += name; // This is the same as greeting = greeting + name;
 console.log(greeting);
 */
-
+/*
 let str1 = 'Hello';
 let str2 = 'World';
 
 let result = str1.concat(' ', str2); 
 console.log(result); // Hello World
+*/
+console.log("********************************************************************");
+
+console.log("Hello, there!"); 
+console.log("I am excited to talk to you!"); 
+let bot;
+bot = "teacherBot";
+let botLocation = "the universe";
+console.log("Allow me to introduce myself.");
+let botIntroduction = "My name is " + bot + ".";
+
+console.log(botIntroduction);
+let botLocationsentence = " I live in " + botLocation + ".";
+console.log(botLocationsentence);
+
+bot = "professorBot";
+console.log("Now, I have a new name: " + bot + ".");
+
+let favSubject = "Mathematics";
+favSubSentence = "My favorite subject is " + favSubject + ".";
+console.log(favSubSentence);
+
---
+++ yoh.js
@@ -0,0 +1,7 @@
+const user = {
+    name: "Kananelo",
+    age: 29,
+    sex: "Male"
+};
+
+console.log(user);
